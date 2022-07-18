faa115cd904f1884b066cf8111e1a93d0d8b3cc8
wenxich <wenxi.c.chen@gmail.com>
2022-07-18 13:22:05 -0400

    Tried to import Blender files but the models are not appearing (head in hands).

--- main.js
+++ main.js
@@ -3,6 +3,7 @@ import './style.css'
 import * as THREE from 'three';
 
 import {GLTFLoader} from "three/examples/jsm/loaders/GLTFLoader";
+import {DRACOLoader} from "three/examples/jsm/loaders/DRACOLoader";
 
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
@@ -14,12 +15,32 @@ document.body.appendChild( renderer.domElement );
 //initiate loader
 const loader = new GLTFLoader();
 
-loader.load( './blender_files/cordyceps.glb', function ( gltf ) {
+//DRACOLoader instance to decode compressed mesh data
+const dracoLoader = new DRACOLoader();
+dracoLoader.setDecoderPath( '/examples/js/libs/draco/' );
+loader.setDRACOLoader( dracoLoader );
 
-    scene.add( gltf.scene );
+loader.load(
+    //resource url
+    './blender_files/cordyceps.glb',
+    //called when resource url loaded
+    function ( gltf ) {
 
-}, undefined, function ( error ) {
+        scene.add( gltf.scene );
 
-    console.error( error );
+    },
 
-} );
+    // called while loading is progressing
+    function ( xhr ) {
+
+        console.log( ( xhr.loaded / xhr.total * 100 ) + '% loaded' );
+
+    },
+
+    // called when loading has errors
+    function () {
+
+        console.log( 'An error happened' );
+
+    }
+);
