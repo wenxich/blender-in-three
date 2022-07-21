eeba451a9b69e75cbe79eae1f43d3d1291889c60
wenxich <wenxi.c.chen@gmail.com>
2022-07-21 14:58:54 -0400

    Blender files are visible!

--- main.js
+++ main.js
@@ -4,43 +4,92 @@ import * as THREE from 'three';
 
 import {GLTFLoader} from "three/examples/jsm/loaders/GLTFLoader";
 import {DRACOLoader} from "three/examples/jsm/loaders/DRACOLoader";
+import {OrbitControls} from "three/examples/jsm/controls/OrbitControls";
 
 const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+const camera = new THREE.PerspectiveCamera( 75, innerWidth / innerHeight, 0.1, 1000 );
 
 const renderer = new THREE.WebGLRenderer();
-renderer.setSize( window.innerWidth, window.innerHeight );
+renderer.setSize( innerWidth, innerHeight );
+renderer.setPixelRatio(devicePixelRatio);
+
+let mesh;
+
 document.body.appendChild( renderer.domElement );
 
-//initiate loader
-const loader = new GLTFLoader();
+//create new OrbitControls object
+new OrbitControls(camera, renderer.domElement);
 
-//DRACOLoader instance to decode compressed mesh data
-const dracoLoader = new DRACOLoader();
-dracoLoader.setDecoderPath( '/examples/js/libs/draco/' );
-loader.setDRACOLoader( dracoLoader );
+//makes what's in front of the black screen visible
+camera.position.z = 5;
 
-loader.load(
-    //resource url
-    './blender_files/cordyceps.glb',
-    //called when resource url loaded
-    function ( gltf ) {
+function init() {
+    scene.background = new THREE.Color('black');
+    camera.position.set(0, 0, 1);
+    renderer.setSize(innerWidth, innerHeight);
+    document.body.appendChild(renderer.domElement);
+}
 
-        scene.add( gltf.scene );
+function setLight() {
+    //parameters: color, intensity
+    const frontLight = new THREE.DirectionalLight(0xC0C0C0, 0.5);
+    frontLight.position.set(0, 0, 1);
+    scene.add( frontLight );
 
-    },
+    //parameters: color, intensity
+    const backLight = new THREE.DirectionalLight(0xC0C0C0, 0.5);
+    backLight.position.set(0, 0, -1);
+    scene.add( backLight );
 
-    // called while loading is progressing
-    function ( xhr ) {
+    //parameters: color, intensity
+    const rightLight = new THREE.DirectionalLight(0xC0C0C0, 0.5);
+    rightLight.position.set(1, 0, 0);
+    scene.add( rightLight );
 
-        console.log( ( xhr.loaded / xhr.total * 100 ) + '% loaded' );
+    //parameters: color, intensity
+    const leftLight = new THREE.DirectionalLight(0xC0C0C0, 0.5);
+    leftLight.position.set(-1, 0, 0);
+    scene.add( leftLight );
 
-    },
+    //parameters: color, intensity
+    const upLight = new THREE.DirectionalLight(0xC0C0C0, 0.5);
+    upLight.position.set(0, 1, 0);
+    scene.add( upLight );
 
-    // called when loading has errors
-    function () {
+    //parameters: color, intensity
+    const downLight = new THREE.DirectionalLight(0xC0C0C0, 1.0);
+    downLight.position.set(0, -1, 0);
+    scene.add( downLight );
+}
+function loadGLTF() {
 
-        console.log( 'An error happened' );
+    let balloonLoader = new GLTFLoader();
 
+    //DRACOLoader instance to decode compressed mesh data
+    const dracoLoader = new DRACOLoader();
+    dracoLoader.setDecoderPath( '/examples/js/libs/draco/' );
+    balloonLoader.setDRACOLoader( dracoLoader );
+
+    balloonLoader.load('blender_files/donut.gltf', (gltf) => {
+        mesh = gltf.scene;
+        mesh.scale.set(5, 5, 5);
+        scene.add(mesh);
+        mesh.position.x = 0;
+        mesh.position.y = 0;
+        mesh.position.z = 0;
+    });
+}
+
+function animate() {
+    requestAnimationFrame(animate);
+    if (mesh && mesh.rotation) {
+        mesh.rotation.y -= 0.005;
+        mesh.rotation.x += 0.005;
     }
-);
+    renderer.render(scene, camera);
+}
+
+init();
+setLight();
+loadGLTF();
+animate();
